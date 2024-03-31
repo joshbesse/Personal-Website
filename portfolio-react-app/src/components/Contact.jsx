6fe849c515d3dc14f5a2264c7f4127f172f3127f
joshbesse <joshua.besse3@gmail.com>
2024-03-31 17:23:54 -0400

    modified:   portfolio-react-app/src/components/Contact.jsx 	modified:   portfolio-react-app/src/components/Navbar.jsx

--- portfolio-react-app/src/components/Contact.jsx
+++ portfolio-react-app/src/components/Contact.jsx
@@ -6,7 +6,7 @@ const Contact = () => {
         <form method='POST' action="https://getform.io/f/8f771eb1-585e-433f-b39b-46dc55f42ce7" className='flex flex-col max-w-[600px] w-full'>
             <div className='pb-8'>
                 <p className='text-4xl font-bold inline border-b-4 border-[#03548A] text-white'>Contact</p>
-                <p className='text-white py-4'>Submit the form below or send me an email - joshua.besse3@gmail.com</p>
+                <p className='text-white py-4'>Submit the form below or send me an email - bessej@bc.edu</p>
             </div>
             <input className='bg-[#ccd6f6] p-2' type="text" placeholder='Name' name='name' />
             <input className='my-4 p-2 bg-[#ccd6f6]' type="email" placeholder='Email' name='email' />
--- portfolio-react-app/src/components/Navbar.jsx
+++ portfolio-react-app/src/components/Navbar.jsx
@@ -90,7 +90,7 @@ const Navbar = () => {
                     </a>
                 </li>
                 <li className='w-[160px] h-[60px] flex justify-between items-cetner ml-[-100px] hover:ml-[-10px] duration-300 bg-[#6fc2b0]'>
-                    <a className='flex justify-between items-center w-full text-gray-300' href='mailto:joshua.besse3@gmail.com'>
+                    <a className='flex justify-between items-center w-full text-gray-300' href='mailto:bessej@bc.edu'>
                         Email <HiOutlineMail size={30}/>
                     </a>
                 </li>
