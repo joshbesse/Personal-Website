b0702b8477efb4e05f717e1e21642736b864e566
joshbesse <joshua.besse3@gmail.com>
2024-10-04 00:42:15 -0400

    updated sentiment project and added algo trading project

--- portfolio-react-app/src/components/Projects.jsx
+++ portfolio-react-app/src/components/Projects.jsx
@@ -4,6 +4,7 @@ import Netflix from '../assets/netflix.png'
 import Fetal from '../assets/fetalhealth.png'
 import Sentiment from '../assets/sentiment.jpg'
 import Sleep from '../assets/sleepincome.webp'
+import AlgoTrading from '../assets/algo_trading.webp'
 
 const Work = () => {        
   return (
@@ -18,6 +19,48 @@ const Work = () => {
         <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-4">
 
             {/* Grid Item */}
+            <div style={{backgroundImage: `url(${AlgoTrading})`}} className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
+                 
+                 {/* Hover Effects */}
+                 <div className="opacity-0 group-hover:opacity-100 ">
+                     <span className="text-2xl font bold text-white">
+                         Algorithmic Trading<br></br> Application
+                     </span>
+                     <div className='pt-8 text-center'>
+                         <a href='https://algo-trading.netlify.app/'>
+                             <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Website</button>
+                         </a>
+                     </div>
+                 </div>
+             </div>
+            <div style={{backgroundImage: `url(${Sentiment})`}} className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
+                 
+                 {/* Hover Effects */}
+                 <div className="opacity-0 group-hover:opacity-100 ">
+                     <span className="text-2xl font bold text-white">
+                         Sentiment Analysis<br></br> Application
+                     </span>
+                     <div className='pt-8 text-center'>
+                         <a href='https://sentiment-analysis-application.netlify.app/'>
+                             <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Website</button>
+                         </a>
+                     </div>
+                 </div>
+             </div>
+            <div style={{backgroundImage: `url(${Fetal})`}} className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
+                 
+                 {/* Hover Effects */}
+                 <div className="opacity-0 group-hover:opacity-100 ">
+                     <span className="text-2xl font bold text-white">
+                         Fetal Health Classification
+                     </span>
+                     <div className='pt-8 text-center'>
+                         <a href='https://github.com/joshbesse/Fetal-Health-Classification/blob/main/Fetal_Health_Notebook.ipynb'>
+                             <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Notebook</button>
+                         </a>
+                     </div>
+                 </div>
+             </div>
             <div style={{backgroundImage: `url(${Relig})`}} className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
                  
                 {/* Hover Effects */}
@@ -44,34 +87,6 @@ const Work = () => {
                             <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Paper</button>
                         </a>
                         <a href='https://github.com/joshbesse/Netflix-Forecasting/blob/main/Netflix_Revenue.R'>
-                            <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
-                        </a>
-                    </div>
-                </div>
-            </div>
-            <div style={{backgroundImage: `url(${Fetal})`}} className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
-                 
-                {/* Hover Effects */}
-                <div className="opacity-0 group-hover:opacity-100 ">
-                    <span className="text-2xl font bold text-white">
-                        Fetal Health Classification
-                    </span>
-                    <div className='pt-8 text-center'>
-                        <a href='https://github.com/joshbesse/Fetal-Health-Classification/blob/main/Fetal_Health_Notebook.ipynb'>
-                            <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Notebook</button>
-                        </a>
-                    </div>
-                </div>
-            </div>
-            <div style={{backgroundImage: `url(${Sentiment})`}} className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
-                 
-                {/* Hover Effects */}
-                <div className="opacity-0 group-hover:opacity-100 ">
-                    <span className="text-2xl font bold text-white">
-                        Sentiment Analysis<br></br> Application
-                    </span>
-                    <div className='pt-8 text-center'>
-                        <a href='https://github.com/joshbesse/Sentiment-Analysis-Application'>
                             <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
                         </a>
                     </div>
